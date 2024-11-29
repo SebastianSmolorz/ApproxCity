cfd8f9598293bd7808dc9859a1a5c5bb77963ba5
sebsmolorz <seb@zenstores.com>
2024-11-29 21:27:22 +0000

    Center

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-<div class="flex flex-col justify-between align-middle gap-10 absolute left-1/3">
+<div class="flex flex-col justify-between text-center gap-10 absolute left-1/3">
   <h1 class="text-4xl">Around The Map</h1>
   <h2 class="text-3xl">Guess a city, town or village nearest to...</h2>
   <NuxtLink to="/closest-city/daily">
