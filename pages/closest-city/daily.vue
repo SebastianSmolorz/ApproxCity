7183508bb3b27c85d9ae6fb429815c320ca5164a
sebsmolorz <seb@zenstores.com>
2024-12-07 13:59:59 +0000

    Link in share

--- pages/closest-city/daily.vue
+++ pages/closest-city/daily.vue
@@ -25,7 +25,7 @@ export default {
     const guessLngLat = ref(null)
     const distance = ref(null)
 
-    const source = computed(() => `NearCity Daily ${gameId.value}\n${distanceToEmojis(lastDailyScore.value)}\nTotal distance: ${lastDailyScore.value}km`)
+    const source = computed(() => `NearCity Daily ${gameId.value}\n${distanceToEmojis(lastDailyScore.value)}\nTotal distance: ${lastDailyScore.value}km\nhttps://approx-city.vercel.app/`)
     const { copy } = useClipboard({ source })
 
     const { share, isSupported } = useShare()
@@ -35,6 +35,7 @@ export default {
         share({
           title: 'NearCity Daily Score',
           text: source.value,
+          url: 'https://approx-city.vercel.app/'
         })
       } else {
         copy()
